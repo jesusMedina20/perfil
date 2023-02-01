a9e008cf8e881627938309307f5014687ff8ef85
jesus medina <jesus.amb0241@gmail.com>
2023-01-31 21:39:46 -0400

    checkin and login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,74 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../hooks/useForm";
 
 export default function Login() {
-  return <div>L</div>;
+  const navigate = useNavigate();
+
+  const { name, email, password, onInputChange, onResetForm } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onRegister = (e) => {
+    e.preventDefault();
+
+    navigate("/perfil", {
+      replace: true,
+      state: {
+        logged: true,
+        name,
+      },
+    });
+
+    onResetForm();
+  };
+  return (
+    <div className="wrapper">
+      <form onSubmit={onRegister}>
+        <h1>Registrarse</h1>
+
+        <div className="input-group">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={onInputChange}
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="name">Nombre:</label>
+        </div>
+
+        <div className="input-group">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={onInputChange}
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="email">Email:</label>
+        </div>
+        <div className="input-group">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={onInputChange}
+            required
+            autoComplete="off"
+          />
+          <label htmlFor="password">Contraseña:</label>
+        </div>
+
+        <button>Registrarse</button>
+      </form>
+    </div>
+  );
 }
